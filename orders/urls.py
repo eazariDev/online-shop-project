9aa5a28c9d7e0e823b4985ae1916985e1f3ad69f
erfan <eazari.dev@gmail.com>
2025-05-24 21:16:40 +0330

    add view, template and url for generating PDF invoices dynamically in administration site

--- orders/urls.py
+++ orders/urls.py
@@ -2,7 +2,7 @@
 
 from django.urls import path
 
-from .views import order_create, admin_order_detail
+from .views import order_create, admin_order_detail, admin_order_pdf
 
 app_name = "orders"
 
@@ -13,5 +13,10 @@ urlpatterns = [
         "admin/order/<int:order_id>/",
         admin_order_detail,
         name="admin_order_detail",
-    )
+    ),
+    
+    path("admin/order/<int:order_id>/pdf/",
+         admin_order_pdf,
+         name='admin_order_pdf'
+        ),
 ]
--- orders/views.py
+++ orders/views.py
@@ -8,7 +8,24 @@ from .models import OrderItem, Order
 
 from .tasks import order_created
 
+import weasyprint
+from django.contrib.staticfiles import finders
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order_id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(
+        response,
+        stylesheets=[weasyprint.CSS(finders.find('css/pdf.css'))]
+    )
+    
+    return response
 
 def order_create(request):
     cart = Cart(request)
